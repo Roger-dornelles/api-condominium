b55461d2819bd67fa0309cbc1910407c904e4987
roger santos <rsd.santo@outlook.com>
2021-12-09 09:47:11 -0300

    procfile add npm start-dev

--- src/instances/mysql.js
+++ src/instances/mysql.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
   process.env.MYSQL_DB,
   process.env.MYSQL_USER,
   process.env.MYSQL_PASSWORD,
+  //process.env.MYSQL_URI,
   {
     dialect:'postgres',
     port:process.env.MYSQL_PORT

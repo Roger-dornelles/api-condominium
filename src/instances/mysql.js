f598728bcb5947974cce9638da37a25fc06da9f8
roger santos <rsd.santo@outlook.com>
2021-12-09 13:36:04 -0300

    Adicionado funcao para converter string para number no process.env.MYSQL_PORT

--- src/instances/mysql.js
+++ src/instances/mysql.js
@@ -3,14 +3,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.MYSQL_DB,
+  process.env.MYSQL_DATABASE,
   process.env.MYSQL_USER,
   process.env.MYSQL_PASSWORD,
   //process.env.MYSQL_URI,
   {
     dialect:'postgres',
-    port:process.env.MYSQL_PORT
+    port:parseInt(process.env.MYSQL_PORT)
   }
 );
+
 
 module.exports = sequelize;
